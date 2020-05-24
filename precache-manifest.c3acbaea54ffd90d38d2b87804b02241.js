05203c8fcc211b43d51c439030123883d714e4f1
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-05-24 11:53:12 +0545

    Updates

---
+++ precache-manifest.c3acbaea54ffd90d38d2b87804b02241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94cc98ea12ecf14c7b9ee3675449c50",
+    "url": "/React-Practice-Projects/index.html"
+  },
+  {
+    "revision": "c429b4f86e7d903f5cab",
+    "url": "/React-Practice-Projects/static/css/main.51a52ae5.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/React-Practice-Projects/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Practice-Projects/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c429b4f86e7d903f5cab",
+    "url": "/React-Practice-Projects/static/js/main.2534512c.chunk.js"
+  },
+  {
+    "revision": "22f5b44ce05e039f541f",
+    "url": "/React-Practice-Projects/static/js/runtime-main.32ea5918.js"
+  }
+]);
