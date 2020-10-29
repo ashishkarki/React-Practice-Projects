500d7a64b5b7176edff11b7d4919cad892b38526
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2020-10-29 17:21:13 +0545

    added objects and looping using map method in react-basics app.

--- fcc-react-smilga/react-basics/src/index.js
+++ fcc-react-smilga/react-basics/src/index.js
@@ -4,31 +4,53 @@ import ReactDom from 'react-dom'
 import './index.css'
 
 // variables
-const imgSrc =
-  'https://images-na.ssl-images-amazon.com/images/I/51x8WaTt84L._AC_SX368_.jpg'
+const books = [
+  {
+    imgSrc:
+      'https://images-na.ssl-images-amazon.com/images/I/51x8WaTt84L._AC_SX368_.jpg',
+    bookName: 'First Booke',
+    author: 'First Authore',
+  },
+  {
+    imgSrc:
+      'https://images-na.ssl-images-amazon.com/images/I/41rbthdtokL._AC_SX368_.jpg',
+    bookName: 'Second Booke',
+    author: 'Second Authore',
+  }
+]
 
 // stateless functional component
 function BookList() {
   return (
     <section className='bookList'>
-      <Book bookName='First Booke' author='First Authore' >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, necessitatibus soluta amet impedit perspiciatis ipsum perferendis modi rem, velit fugit natus enim quas, deserunt exercitationem atque? Repellendus ab porro at!
-      </Book>
-
-      <Book bookName='Second Booke' author='Second Authore' />
+      {
+        books.map((book, index) =>
+          <Book
+            key={ index }
+            // imgSrc={ book.imgSrc }
+            // bookName={ book.bookName }
+            // author={ book.author }
+            // book={ book }
+            { ...book }
+          />
+        )
+      }
     </section>
   )
 }
 
-const Book = ({ bookName, author, children }) => {
+const Book = ({ imgSrc, bookName, author }) => { //props
+  // const { imgSrc, bookName, author } = props.book
+  // const { imgSrc, bookName, author } = props
+
   return (
     <article className='book'>
-      <img src={ imgSrc } alt='image' height='90px' />
+      <img src={ imgSrc } height='90px' />
       <h1 style={ { color: 'silver' } }>{ bookName }</h1>
       <h4 className='author'>{ author }</h4>
-      <p>
-        { children }
-      </p>
+      {/* <p>
+        { props.children }
+      </p> */}
     </article>
   )
 }
